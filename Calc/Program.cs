c6bc087ec93f2183de8a8eb0e873c4afbf9103c9
alinnok <56650500+alinnok@users.noreply.github.com>
2019-10-22 18:51:17 +0300

    Добавьте файлы проекта.

---
+++ Calc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine($"Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите операцию (+,-,*,/)");
+            string oper = Console.ReadLine();
+            Console.WriteLine($"Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+            //int result;
+
+
+            switch (oper)
+            {
+                case "+":
+                    {
+                        var result = a + b;
+                        Console.WriteLine($"{a} + {b} = {result}");
+                        break;
+
+                    }
+                case "-":
+                    {
+                        var result = a - b;
+                        Console.WriteLine($"{a} - {b} = {result}");
+                        break;
+                    }
+                case "*":
+                    {
+                        var result = a * b;
+                        Console.WriteLine($"{a} * {b} = {result}");
+                        break;
+                    }
+                case "/":
+                    {
+                        var result = a / b;
+                        Console.WriteLine($"{a} / {b} = {result}");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine($"Набрана неизвестная операция");
+                        break;
+                    }
+
+            }
+
+        }
+    }
+}
